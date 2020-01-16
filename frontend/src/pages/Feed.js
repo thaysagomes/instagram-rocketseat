844334e739b25812457b37f8e3e5873502c17d35
Thaysa Gomes <thaysacgms@gmil.com>
2020-01-16 19:27:31 -0300

    comunicação da api com o feed

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,26 +1,38 @@
 import React, {Component} from 'react';
 import './Feed.css';
+import api from '../services/api';
 import more from '../assets/more.svg';
 import like from '../assets/like.svg';
 import send from '../assets/send.svg';
 import comment from '../assets/comment.svg';
 
 class Feed extends Component {
+
+    state = { //variável para armazenar informações para serem alteradas
+        feed: [],
+    };
+
+    async componentDidMount(){
+        const response = await api.get('posts'); //o que é passado dentro dos parênteses vai funcionar junto com a url, ex: 'localhost:3333/posts'
+
+        this.setState({ feed: response.data });
+    }
     render() {
         return(
             <section id="post-list">
-                <article>
+                {this.state.feed.map( post => (
+                    <article>
                     <header>
                         <div className="user-info">
-                            <span>Thyago Gomes</span>
-                            <span className="place">Rio de Janeiro</span>
+                            <span>{post.author}</span>
+                            <span className="place">{post.place}</span>
                         </div>
 
                         <img src={more} alt="Mais informações"/>
 
                     </header>
                     
-                    <img src="http://localhost:3333/files/20180103_171608.jpg"/>
+                    <img src={`http://localhost:3333/files/${post.image}`}/>
 
                     <footer>
                         <div className="actions">
@@ -28,36 +40,12 @@ class Feed extends Component {
                             <img src={comment} alt="Comentários"/>
                             <img src={send} alt="Compartilhar publicação"/>
                         </div>
-                        <strong>20 curtidas</strong>
-                        <p>Esse dia foi massa! <span>#riodejaneiro #rocketseat</span></p>
+                        <strong>{post.likes} curtidas</strong>
+                        <p>{post.description}<span>{post.hashtags}</span></p>
                     </footer>
 
                 </article>
-                
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Thyago Gomes</span>
-                            <span className="place">Rio de Janeiro</span>
-                        </div>
-
-                        <img src={more} alt="Mais informações"/>
-
-                    </header>
-                    
-                    <img src="http://localhost:3333/files/20180103_171608.jpg"/>
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="Curtidas"/>
-                            <img src={comment} alt="Comentários"/>
-                            <img src={send} alt="Compartilhar publicação"/>
-                        </div>
-                        <strong>20 curtidas</strong>
-                        <p>Esse dia foi massa! <span>#riodejaneiro #rocketseat</span></p>
-                    </footer>
-
-                </article>
+                ))}
             </section>
         );
     }
