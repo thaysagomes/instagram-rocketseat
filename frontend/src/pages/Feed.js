9c445e6a8d8cfda2cb3bcce21accc4071f94d992
Thaysa Gomes <thaysacgms@gmil.com>
2020-01-16 20:53:49 -0300

    finalização da aplicação em react

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './Feed.css';
 import api from '../services/api';
+import io from 'socket.io-client';
 import more from '../assets/more.svg';
 import like from '../assets/like.svg';
 import send from '../assets/send.svg';
@@ -13,15 +14,42 @@ class Feed extends Component {
     };
 
     async componentDidMount(){
+
+        this.registerToSocket();
+
         const response = await api.get('posts'); //o que é passado dentro dos parênteses vai funcionar junto com a url, ex: 'localhost:3333/posts'
 
         this.setState({ feed: response.data });
     }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        //duas informações em real time: post e like
+
+        socket.on('post', newPost => {
+            this.setState({feed: [newPost, ...this.state.feed]}); //newPost sendo o primeiro da lista e o resto vindo sucessivamente
+        });
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post =>
+                    post._id === likedPost._id ? likedPost : post
+                )
+            })
+        });
+    }
+
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`); //a mesma rota que é utilizada na api
+    }
+
     render() {
         return(
             <section id="post-list">
-                {this.state.feed.map( post => (
-                    <article>
+                {this.state.feed.map( post => ( //sempre que utilizar o map, deve-se colocar o key no primeiro elemento que retornar
+                    <article key={post._id}>  
                     <header>
                         <div className="user-info">
                             <span>{post.author}</span>
@@ -36,8 +64,11 @@ class Feed extends Component {
 
                     <footer>
                         <div className="actions">
+                            <button type="button" onClick={ () => this.handleLike(post._id)}>
                             <img src={like} alt="Curtidas"/>
+                            <button/>
                             <img src={comment} alt="Comentários"/>
+                            </button>
                             <img src={send} alt="Compartilhar publicação"/>
                         </div>
                         <strong>{post.likes} curtidas</strong>
--- frontend/src/pages/NewPhoto.js
+++ frontend/src/pages/NewPhoto.js
@@ -1,9 +1,83 @@
 import React, {Component} from 'react';
+import './NewPhoto.css';
+import api from '../services/api';
 
 class NewPhoto extends Component {
+    
+    state = {
+        image: null,
+        author: '',
+        place: '',
+        description: '',
+        hashtags: '',
+    };
+
+    handleChange = e => { //função para pegar o que for escrito no input (e = evento)
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    handleImageChange = e => {
+        this.setState({ image: e.target.files[0] });
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault(); //evita que atualize a página ao submeter o form
+
+        const data = new FormData(); //como não está utilizando json, deve-se instanciar o FormData
+
+        data.append('image', this.state.image);
+        data.append('author', this.state.author);
+        data.append('place', this.state.place);
+        data.append('description', this.state.description);
+        data.append('hashtags', this.state.hashtags);
+
+        await api.post('posts', data);
+
+        this.props.history.push('/'); //direcionar o usuário para uma rota específica
+    }
+
     render() {
         return(
-            <h1>New Photo</h1>
+            <form id="new-post" onSubmit={this.handleSubmit}>
+                <input type="file" onChange={this.handleImageChange} />
+
+                <input
+                type="text"
+                name="author"
+                placeholder="Autor do post"
+                onChange={this.handleChange}
+                value={this.state.author}
+                />
+
+                
+                <input
+                type="text"
+                name="place"
+                placeholder="Local do post"
+                onChange={this.handleChange}
+                value={this.state.place}
+                />
+
+                
+                <input
+                type="text"
+                name="description"
+                placeholder="Descrição do post"
+                onChange={this.handleChange}
+                value={this.state.description}
+                />
+
+                
+                <input
+                type="text"
+                name="hashtags"
+                placeholder="Hashtags do post"
+                onChange={this.handleChange}
+                value={this.state.hashtags}
+                />
+
+                <button type="submit">Enviar</button>
+            </form>
         );
     }
 }
